530ade20536391cc6eeafc5c9ea65792196be7d9
Phil Newton <phil@sodaware.net>
2020-02-11 17:01:42 -0500

    Reduce requirements to Emacs 24.1
    24.4 was required for `with-eval-after-load`, but this has been removed and
    requires user action to enable.

--- blitzmax-mode.el
+++ blitzmax-mode.el
@@ -6,7 +6,7 @@
 ;; Keywords: languages blitzmax
 ;; Author: Phil Newton
 ;; URL: https://www.sodaware.net/dev/tools/blitzmax-mode/
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "24.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
