5f67bb3c8e4baf1f6881cc998f9f031641a7b08a
Phil Newton <phil@sodaware.net>
2020-04-15 11:29:10 -0400

    Add configuration info to module documentation

--- blitzmax-mode.el
+++ blitzmax-mode.el
@@ -34,13 +34,32 @@
 ;; This is a major mode for editing BlitzMax files.  It supports syntax
 ;; highlighting, keyword capitalization, and automatic indentation.
 
-;; If you want to use quickrun integration, add the following code to your Emacs
+;; blitzmax-mode provides the following configuration options:
+
+;; blitzmax-mode-indent ::
+;;   The number of spaces to indent by.  By default blitzmax-mode indents by 4
+;;   spaces which is converted to a single tab.
+
+;; blitzmax-mode-capitalize-keywords-p ::
+;;   Disable automatic capitalization of keywords by setting this to =nil=.  =t=
+;;   by default.
+
+;; blitzmax-mode-smart-indent-p
+;;   Disable smart indentation by setting this to =nil=.  =t= by default.
+
+;; blitzmax-mode-compiler-pathname
+;;   Full pathname to the BlitzMax compiler bmk.  "bmk" by default.
+
+;; To enable quickrun integration, add the following code to your Emacs
 ;; initialization file.
 
 ;;   (with-eval-after-load 'quickrun
 ;;     (blitzmax-mode-quickrun-integration))
 
-;;; Configuration:
+;;; Code:
+
+;; --------------------------------------------------
+;; -- Configuration
 
 (defgroup blitzmax nil
   "Major mode for editing BlitzMax source files."
@@ -72,7 +91,6 @@
   :type 'hook
   :group 'hook)
 
-;;; Code:
 
 ;; --------------------------------------------------
 ;; -- Local Variables
